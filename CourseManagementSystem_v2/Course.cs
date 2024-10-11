e1a0f5233bd74f0703e27bfc7b99a153f0330f2c
UT01228 <UT01228@UNICOMTIC25>
2024-10-11 15:57:44 +0530

    Task 11 : Create a Class name is Course

---
+++ CourseManagementSystem_v2/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v2
+{
+    public class Course
+    {
+        public string CourseId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Duration { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
--- CourseManagementSystem_v2/Program.cs
+++ CourseManagementSystem_v2/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CourseManagementSystem_v2
 {
-     public class Program
+      class Program
      {
         static void Main(string[] args)
         {
